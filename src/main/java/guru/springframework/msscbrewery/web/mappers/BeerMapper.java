b4257e5105bda7316bbb57f935e51e592e26a28a
Ileana Oneata <ileana.oneata@yahoo.com>
2022-05-09 18:23:50 +0300

    Example of using Project Lombok and MapStruct

---
+++ src/main/java/guru/springframework/msscbrewery/web/mappers/BeerMapper.java
@@ -0,0 +1,17 @@
+package guru.springframework.msscbrewery.web.mappers;
+
+import guru.springframework.msscbrewery.domain.Beer;
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.mapstruct.Mapper;
+
+/**
+ * @author ileanaoneata on 09.05.2022
+ */
+
+@Mapper
+public interface BeerMapper {
+
+    BeerDto beerToBeerDto(Beer beer);
+
+    Beer beerDtoToBeer(BeerDto dto);
+}
